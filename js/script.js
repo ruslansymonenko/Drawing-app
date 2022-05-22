00747d6f83495575a76d4ce5129e5870173448d0
ruslansymonenko <symonenkors@gmail.com>
2022-05-22 18:46:38 +0300

    Created the basic app's functions

---
+++ js/script.js
@@ -0,0 +1,116 @@
+const canvas = document.querySelector('#jsCanvas');
+const ctx = canvas.getContext('2d');
+const colors = document.querySelectorAll('.jsColor');
+const range = document.querySelector('#jsRange');
+const mode = document.querySelector('#jsMode');
+const saveBtn = document.querySelector('#jsSave');
+
+const initialColor = '#2c2c2c';
+const canvasSize = 700;
+
+canvas.height = canvasSize;
+canvas.width = canvasSize;
+
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvasSize, canvasSize);
+
+ctx.lineWidth = 2.5;
+ctx.strokeStyle = initialColor;
+ctx.fillStyle = initialColor;
+
+// ctx.fillStyle = 'green'
+// ctx.fillRect(100, 100, 300, 450);
+
+let painting = false;
+let filling = false;
+
+function stopPainting () {
+    painting = false;
+}
+
+function startPainting () {
+    painting = true;
+}
+
+function onMouseMove (event) {
+    x = event.offsetX;
+    y = event.offsetY;
+
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke()
+    }
+}
+
+function onMouseDown (event) {
+    if (filling === false) {
+        painting = true;
+    }
+}
+
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange (event) {
+    const rangeValue = event.target.value;
+    ctx.lineWidth = rangeValue;
+}
+
+function handleModeClick () {
+    if (filling === true) {
+        filling = false;
+        mode.innerHTML = 'Заливка';
+    } else {
+        filling = true;
+        mode.innerHTML = 'Рисование';
+    }
+}
+
+function handleCanvasClick () {
+    if (filling) {
+        ctx.fillRect(0, 0, canvasSize, canvasSize);
+    }
+}
+
+function handleCM (event) {
+    event.preventDefault();
+}
+
+function handleSaveClick () {
+    const image = canvas.toDataURL();
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'my drawing';
+    link.click();
+}
+
+
+if (canvas) {
+    canvas.addEventListener('mousemove', onMouseMove);
+    canvas.addEventListener('mousedown', onMouseDown);
+    canvas.addEventListener('mouseup', stopPainting);
+    canvas.addEventListener('mouseleave', stopPainting);
+    canvas.addEventListener('click', handleCanvasClick);
+    canvas.addEventListener('contextmenu', handleCM);
+}
+
+
+Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
+
+if (range) {
+    range.addEventListener('input', handleRangeChange);
+}
+
+if (mode) {
+    mode.addEventListener('click', handleModeClick);
+}
+
+if (saveBtn) {
+    saveBtn.addEventListener('click', handleSaveClick);
+}
